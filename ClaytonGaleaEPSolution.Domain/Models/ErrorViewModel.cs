1ec7168b0d0083b9bfb6ca4cd8f9a73782623ce4
Clayton Galea <claytongalea78@outlook.com>
2025-04-04 18:31:15 +0200

    Commit with the Layers 3

---
+++ ClaytonGaleaEPSolution.Domain/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ClaytonGaleaEPSOlution.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
